7f041788babbc13b3d8eb4583f0f65029913e8f3
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-08-19 14:07:00 -0300

    +Clase principal de aplicacion +Funcion para ingreso de contacto nulo

---
+++ src/agenda/AgendaApp.java
@@ -0,0 +1,10 @@
+package agenda;
+
+public class AgendaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/agenda/Contacto.java
+++ src/agenda/Contacto.java
@@ -1,9 +1,17 @@
 package agenda;
 
+//Declaro variables
 public class Contacto {
 	private String nombre;
 	private String apellido;
 	private String email;
 	private String movil;
-	
+
+//Funcion para el caso de que el contacto ingresado sea nulo.
+	public Contacto(){
+		this.nombre=null;
+		this.apellido=null;
+		this.email=null;
+		this.movil=null;
+	}
 }
