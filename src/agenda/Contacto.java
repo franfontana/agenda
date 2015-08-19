84e875e704aa0a782844adc7c2b5aba8af043381
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-08-19 14:29:39 -0300

    +Funcion para ingreso de cualquier contacto

--- src/agenda/Contacto.java
+++ src/agenda/Contacto.java
@@ -1,17 +1,27 @@
 package agenda;
 
-//Declaro variables
+
 public class Contacto {
+
+//Declaro variables
 	private String nombre;
 	private String apellido;
 	private String email;
 	private String movil;
 
-//Funcion para el caso de que el contacto ingresado sea nulo.
+//Funcion para cuando no se ingresa contacto.
 	public Contacto(){
 		this.nombre=null;
 		this.apellido=null;
 		this.email=null;
 		this.movil=null;
+	}
+	
+//Funcion para ingreso de cualquier contacto.
+	public Contacto(String nombre, String apellido, String email, String movil){
+		this.nombre=nombre;
+		this.apellido=apellido;
+		this.email=email;
+		this.movil=movil;
 	}
 }
