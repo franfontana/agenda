2c79d1cf01e79e06779d448e2dc74d391c20c7e4
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-08-20 14:27:17 -0300

    +Notificacion de comienzo de App

--- src/agenda/AppPrincipal.java
+++ src/agenda/AppPrincipal.java
@@ -1,10 +1,13 @@
 package agenda;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class AppPrincipal {
 
 	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Agenda Personal");
 		Scanner teclado=new Scanner(System.in);
 		
 
