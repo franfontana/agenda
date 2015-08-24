737d1b221acab600e54833e59d532c9195886a76
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-08-24 12:40:59 -0300

    Modifico la condicion while del menu para no reiterar codigo absurdo

--- src/agenda/AppPrincipal.java
+++ src/agenda/AppPrincipal.java
@@ -20,7 +20,7 @@ public class AppPrincipal {
 		String textAux;
 		int opcion=1;												//Primer ingreso al bucle del menu.
 		
-		while ((opcion==1) || (opcion==2) || (opcion==3) || (opcion==4) || (opcion==5) || (opcion==6)){
+		while ((opcion<=6) || (opcion==0)){
 			
 			textAux=JOptionPane.showInputDialog("MENU DE OPCIONES:\nIngrese la opcion a realizar:\n1..Nuevo Contacto\n2..Buscar Contacto\n3..Modificar Contacto\n4..Eliminar Contacto\n5..Listado de Contactos\n6..Eliminar TODOS los contactos de la Agenda\n0..Salir de la Agenda");
 			opcion=Integer.parseInt(textAux);							
