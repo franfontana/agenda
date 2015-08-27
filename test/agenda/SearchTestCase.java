edb8d956b19339bb71f2a9b91c8d1ae970630b93
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-08-27 15:52:00 -0300

    Se genera Source Folder Test + SearchTestCase.java

---
+++ test/agenda/SearchTestCase.java
@@ -0,0 +1,14 @@
+package agenda;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SearchTestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
