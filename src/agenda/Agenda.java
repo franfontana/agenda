d313b6f70d4a86ca65de950a3dbef2c86efb3ac4
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-10-07 12:42:17 -0300

    Se modifica metodo agregarContacto agregando condicion if y simplificando lineas para ahorro de codigo. Se agregan metodos para completar los test.

--- src/agenda/Agenda.java
+++ src/agenda/Agenda.java
@@ -20,19 +20,20 @@ public class Agenda {
 		ArrayList<Contacto> contactosEncontrados = buscarContacto(email);
 		if(contactosEncontrados.size() == 0){
 			Contacto unContacto1 = new Contacto(nombre, apellido, email, movil);
-			this.listaDeContactos.add(unContacto1);
-			System.out.println("\nContacto ingresado correctamente:\n" + "Nombre:" + unContacto1.getNombre() + "\t" + "Apellido:" + unContacto1.getApellido() + "\t" + "Email:" + unContacto1.getEmail() + "\t" + "Movil:" + unContacto1.getMovil() + "\n");
+			if(this.listaDeContactos.add(unContacto1)) retorno = true; else retorno = false;				
+		}else retorno = false;		
+		return retorno;
+	}
+	
+	public boolean eliminarContacto(String nombre, String apellido, String email, String movil){
+		boolean retorno;
+		Contacto unContacto2 = new Contacto(nombre, apellido, email, movil);
+		if(this.listaDeContactos.remove(unContacto2)){
 			retorno = true;
 		}else{
 			retorno = false;
 		}
 		return retorno;
-	}
-	
-	public boolean eliminarContacto(String nombre, String apellido, String email, String movil){
-		Contacto unContacto2 = new Contacto(nombre, apellido, email, movil);
-		this.listaDeContactos.remove(unContacto2);
-		return true;
 	}
 	
 	public ArrayList<Contacto> buscarContacto(String busqueda){
@@ -76,10 +77,48 @@ public class Agenda {
 			System.out.println("\nContactos Encontrados:");
 			System.out.println(mostrarBusqueda);
 		}
-		if(auxList.size() == 0){
-			System.out.println("No existe contacto con dicho atributo.");
-		}
 		return auxList;
+	}
+	
+	public boolean modificarContacto(String nombre, String apellido, String email, String movil, String nuevoNombre, String nuevoApellido, String nuevoEmail, String nuevoMovil) {
+		ArrayList<Contacto> auxList2 = new ArrayList<Contacto>();
+		boolean retorno = false;
+		auxList2 = buscarContacto(nuevoEmail);
+			if(auxList2.size() == 0 || nuevoEmail == email){
+				eliminarContacto(nombre, apellido, email, movil);
+			}else{
+				retorno = false;
+			}			
+		retorno = agregarContacto(nuevoNombre, nuevoApellido, nuevoEmail, nuevoMovil);
+		if(retorno == false){
+			System.out.println("\nNo se ha modificado el contacto.");
+		}
+		return retorno;
+	}
+	
+	public boolean mostrarTodosLosContactos(){
+		boolean retorno;
+		if(listaDeContactos.size() > 0){
+			retorno = true;			
+			for(Contacto c : listaDeContactos){
+				System.out.println("Nombre:" + c.getNombre() + "\t" + "Apellido:" + c.getApellido() + "\t" + "Email:" + c.getEmail() + "\t" + "Movil:" + c.getMovil());
+			}
+		}else{
+			retorno  = false;
+		}
+		return retorno;
+	}
+	
+	public boolean eliminarTodosLosContactos(){
+		boolean retorno;
+		if(listaDeContactos.size() > 0){
+			retorno = true;
+			listaDeContactos.removeAll(listaDeContactos);
+			System.out.println("Todos los contactos han sido eliminados.");
+		}else{
+			retorno = false;
+		}
+		return retorno;
 	}
 	
 }
