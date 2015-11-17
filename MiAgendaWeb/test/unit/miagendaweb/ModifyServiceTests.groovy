bf2878d71e68f12858388856c09da119fbaf9fc3
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-11-17 17:44:17 -0300

    Se agrega test unit testModificaAtributosDeContactoExistente() en ModifyServiceTests

--- MiAgendaWeb/test/unit/miagendaweb/ModifyServiceTests.groovy
+++ MiAgendaWeb/test/unit/miagendaweb/ModifyServiceTests.groovy
@@ -1,8 +1,19 @@
 package miagendaweb
 
 import grails.test.*
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static java.util.UUID.randomUUID;
+import static java.util.Random.*;
 
 class ModifyServiceTests extends GrailsUnitTestCase {
+	
+	String nombreTest = randomUUID() as String;
+	String apellidoTest = randomUUID() as String;
+	String emailTest = randomUUID() as String;
+	String movilTest = randomUUID() as String;
+	ModifyService modifyService;
+	
     protected void setUp() {
         super.setUp()
     }
@@ -11,7 +22,19 @@ class ModifyServiceTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 
-    void testSomething() {
+    void testModificaAtributosDeContactoExistente() {
 
+		modifyService = new ModifyService();
+		modifyService.searchService = new SearchService();
+		Contacto contacto = new Contacto(nombreTest, apellidoTest, emailTest, movilTest);
+		String emailDeContactoAModificar = emailTest;
+		
+		String[] atributoAModificar = ["Nombre", "Apellido", "Email", "Movil"];
+		
+		int numRandom = Math.round(Math.random());
+		
+		String nuevoValorDeAtributo = randomUUID() as String;
+		modifyService.searchService.metaClass {buscarUnContacto() {String email -> return contacto}};
+		assertTrue(modifyService.modificarContacto(emailDeContactoAModificar, atributoAModificar[numRandom], nuevoValorDeAtributo));
     }
 }
