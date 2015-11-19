6930425f07d5146ff539f71e217e9a76ce033ae0
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-11-19 15:18:53 -0300

    Se agrega test unit testBusquedaContactoInexistente() en SearchServiceTests

--- MiAgendaWeb/test/unit/miagendaweb/SearchServiceTests.groovy
+++ MiAgendaWeb/test/unit/miagendaweb/SearchServiceTests.groovy
@@ -1,8 +1,17 @@
 package miagendaweb
 
+import static org.junit.Assert.assertEquals;
 import grails.test.*
+import static java.util.UUID.randomUUID;
 
 class SearchServiceTests extends GrailsUnitTestCase {
+	
+	String nombreTest = randomUUID() as String;
+	String apellidoTest = randomUUID() as String;
+	String emailTest = randomUUID() as String;
+	String movilTest = randomUUID() as String;
+	SearchService searchService;
+	
     protected void setUp() {
         super.setUp()
     }
@@ -11,7 +20,12 @@ class SearchServiceTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 
-    void testSomething() {
-
-    }
+	void testBusquedaContactoInexistente(){		
+		searchService = new SearchService();
+		searchService.dataService = new DataService();
+		String atributoTest = randomUUID() as String;
+		Contacto contacto = new Contacto(nombreTest, apellidoTest, emailTest, movilTest);
+		searchService.dataService.listaDeContactos.add(contacto);		
+		assertEquals(null, searchService.buscarUnContacto(atributoTest));
+	}
 }
