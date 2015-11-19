019d7a036bfa6e1d963f875ae204f16bec4ec914
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-11-19 16:04:51 -0300

    Se agrega test unit testBusquedaDeContactosExistentesPorMovilConIgualNombreYApellido() en SearchServiceTests

--- MiAgendaWeb/test/unit/miagendaweb/SearchServiceTests.groovy
+++ MiAgendaWeb/test/unit/miagendaweb/SearchServiceTests.groovy
@@ -77,4 +77,14 @@ class SearchServiceTests extends GrailsUnitTestCase {
 		String contactosEncontrados = searchService.buscarContactosMedianteAlgunAtributo(atributoTest);
 		assertTrue(contactosEncontrados.length()>0);
 	}
+	
+	void testBusquedaDeContactosExistentesPorMovilConIgualNombreYApellido(){
+		searchService = new SearchService();
+		searchService.dataService = new DataService();
+		String atributoTest = movilTest;
+		Contacto contacto = new Contacto(nombreTest, nombreTest, emailTest, movilTest);
+		searchService.dataService.listaDeContactos.add(contacto);
+		String contactosEncontrados = searchService.buscarContactosMedianteAlgunAtributo(atributoTest);
+		assertTrue(contactosEncontrados.length()>0);
+	}
 }
