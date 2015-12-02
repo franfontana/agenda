25528a64964d86a23a9b7359bdd0fe34c1901891
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-12-02 15:19:09 -0300

    Se modifica configuracion de UrlMappings para porder generar los servicios REST para cada accion de la agenda. Funcionan con curl: GET POST DELETE devolviendo la gsp asociada y realizando la accion solicitada

--- MiAgendaWeb/grails-app/conf/UrlMappings.groovy
+++ MiAgendaWeb/grails-app/conf/UrlMappings.groovy
@@ -1,6 +1,53 @@
 class UrlMappings {
 
 	static mappings = {
+		
+//		"/list" (controller:"list", parseRequest:true){
+//			action = [GET:"mostrarTodosLosContactos", PUT:"update", DELETE:"delete", POST:"save"]	//Sin Parametro
+//		}
+		
+		"/list" (controller:"list", parseRequest:true){
+			action = [GET:"mostrarTodosLosContactos"]	//Sin Parametro
+		}
+		
+		"/search" (controller:"search", parseRequest:true){
+			action = [GET:"buscarContactoPorEmailConResultado"]		//Parametro "Email"
+		}
+		
+		"/search" (controller:"search", parseRequest:true){
+			action = [GET:"busquedaPorExpresionConResultado"]		//Parametro "Expresion"
+		}
+		
+		"/create" (controller:"create", parseRequest:true){
+			action = [POST:"agregarContactoConResultado"]			//Parametro "Nombre""Apellido""Email""Movil"
+		}
+		
+//		"/modify" (controller:"modify", parseRequest:true){
+//			action = [PUT:"modificarNombreConResultado"]			//Parametro "Nombre"
+//		}
+		
+//		"/modify" (controller:"modify", parseRequest:true){
+//			action = [PUT:"modificarContactoConInforme"]			//Parametro "Email"
+//		}
+		
+		"/deleteall" (controller:"delete", parseRequest:true){
+			action = [DELETE:"eliminarTodosLosContactosOK"]			//Parametro "Afirmacion"
+		}
+		
+		"/delete" (controller:"delete", parseRequest:true){
+			action = [DELETE:"eliminarContactoConResultadoYValidacion"]		//Parametro "Afirmacion""Email"
+		}
+		
+		
+		
+		
+		
+		
+		
+//		"/createContact" (service:"create", parseRequest){
+//			action = [POST:"agregarContacto"]
+//		}
+		
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
