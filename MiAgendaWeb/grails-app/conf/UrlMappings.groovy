ba6cee4a0f07ea9e5c9ce1f0f632b564d71e123f
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-12-18 17:09:22 -0300

    Se corrige linea comentada: nombre de controller allcontacts en mapeo de URLMappins

--- MiAgendaWeb/grails-app/conf/UrlMappings.groovy
+++ MiAgendaWeb/grails-app/conf/UrlMappings.groovy
@@ -61,7 +61,7 @@ class UrlMappings {
 		
 		
 		
-//		"/api/contacts" (controller: "contacts", parseRequest:true){
+//		"/api/allcontacts" (controller: "allcontacts", parseRequest:true){
 //			action = [GET:"listContacts"]
 //		}
 		
